a6192bf7d95eed555fe1eaf56a4abdcd9c248561
Armed Mob <j1034t@gmail.com>
2022-02-05 18:28:13 -0600

    Buiding More Services
    Async Response Services

--- src/PlannerApp/Program.cs
+++ src/PlannerApp/Program.cs
@@ -1,3 +1,4 @@
+using Blazored.LocalStorage;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
@@ -7,6 +8,7 @@ using System;
 using System.Collections.Generic;
 using System.Net.Http;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 
 namespace PlannerApp
@@ -18,11 +20,39 @@ namespace PlannerApp
             var builder = WebAssemblyHostBuilder.CreateDefault(args);
             builder.RootComponents.Add<App>("#app");
 
-            builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+            //builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+
+            builder.Services.AddHttpClient("PlannerApp.Api", client =>
+            {
+                client.BaseAddress = new Uri("https://plannerapp-api.azurewebsites.net");
+            }).AddHttpMessageHandler<AuthorizationMessageHandler>();
 
             builder.Services.AddMudServices();
+            builder.Services.AddBlazoredLocalStorage();
 
             await builder.Build().RunAsync();
+        }
+    }
+
+    public class AuthorizationMessageHandler : DelegatingHandler
+    {
+
+        private readonly ILocalStorageService _storage;
+
+        public AuthorizationMessageHandler(ILocalStorageService storage)
+        {
+            _storage = storage;
+        }
+
+        protected async override Task<HttpResponseMessage> SendAsync(HttpRequestMessage request, CancellationToken cancelationToken)
+        {
+            if (await _storage.ContainKeyAsync("access_token"))
+            {
+                var token = await _storage.GetItemAsStringAsync("access_token");
+                request.Headers.Authorization = new System.Net.Http.Headers.AuthenticationHeaderValue("Bearer", token);
+            }
+
+            return await base.SendAsync(request, cancelationToken);
         }
     }
 }
